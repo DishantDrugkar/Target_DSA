6609f6ba192e31ecce87c9ffd4b1768d5dabc774
DishantDrugkar <dishantdrugkar4@gmail.com>
2025-03-15 11:07:37 +0530

    Initial

--- src/main/java/org/example/Leetcode_500/Leetcode_1021_Remove_Outermost_Parenthesis.java
+++ src/main/java/org/example/Leetcode_500/Leetcode_1021_Remove_Outermost_Parenthesis.java
@@ -1,4 +1,27 @@
 package org.example.Leetcode_500;
 
 public class Leetcode_1021_Remove_Outermost_Parenthesis {
+    public static void main(String[] args) {
+        String str = "(()())(())";
+        System.out.println(removeParenthesis(str));
+    }
+    public static String removeParenthesis(String str) {
+        int counter = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch == '(') {
+                if (counter > 0) {
+                    sb.append(ch);
+                }
+                counter++;
+            } else{
+                counter--;
+                if (counter > 0) {
+                    sb.append(ch);
+                }
+            }
+        }
+        return sb.toString();
+    }
 }
