3bcfe92e3aea4a5ca824f9c1c03c6047e48a45de
DishantDrugkar <dishantdrugkar4@gmail.com>
2025-03-15 11:05:04 +0530

    Next permutation

---
+++ src/main/java/org/example/Leetcode_500/Leetcode_1021_Remove_Outermost_Parenthesis.java
@@ -0,0 +1,4 @@
+package org.example.Leetcode_500;
+
+public class Leetcode_1021_Remove_Outermost_Parenthesis {
+}
---
+++ src/main/java/org/example/Leetcode_500/Leetcode_31_Next_Permutation.java
@@ -0,0 +1,4 @@
+package org.example.Leetcode_500;
+
+public class Leetcode_31_Next_Permutation {
+}
